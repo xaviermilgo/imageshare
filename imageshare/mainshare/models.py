1bca493491ed0a7cd88dba540b96f96eb3ab33ec
Xavier Kibet <xavierkibet@gmail.com>
2018-07-19 21:54:54 +0300

    Add image model

--- imageshare/mainshare/models.py
+++ imageshare/mainshare/models.py
@@ -6,3 +6,10 @@ class Category(models.Model):
     Name = models.CharField(max_length=30)
     def __str__(self):
         return self.Name
+class Image(models.Model):
+    Name = models.CharField(max_length =60)
+    description = models.TextField(default=Deafult_desc)
+    category = models.ForeignKey(Category, related_name="images")
+    location = models.ForeignKey(Location)
+    submited = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to = 'images/')
