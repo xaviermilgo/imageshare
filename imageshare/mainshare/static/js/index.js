89d577b5149444a1cf3c23d87d79d031797fdad0
Xavier Kibet <xavierkibet@gmail.com>
2018-07-19 22:04:17 +0300

    Add show modal function

---
+++ imageshare/mainshare/static/js/index.js
@@ -0,0 +1,15 @@
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top - 50
+    }, 500);
+});
+showinfo = (i)=>{
+  dict=eval(i)
+  $('#imageimage').attr('src',dict.image);
+  $('#imagedescription').text(dict.desc);
+  $('#imagetitle').text(dict.name);
+  $("#imagelink").val(window.location.origin+'/#'+dict.id);
+  $("#ImageModal").modal('show');
+}
