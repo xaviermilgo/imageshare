40c0f0de9b94b28a5aa6285b8508f7b647f16e68
Xavier Kibet <xavierkibet@gmail.com>
2018-07-19 21:50:55 +0300

    Include models to admin page

--- imageshare/mainshare/admin.py
+++ imageshare/mainshare/admin.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Image)
