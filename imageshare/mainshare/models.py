84d4c5edaca3abe54b892580a08b8139cf1145ca
Xavier Kibet <xavierkibet@gmail.com>
2018-07-19 21:54:31 +0300

    Add Category models

--- imageshare/mainshare/models.py
+++ imageshare/mainshare/models.py
@@ -2,3 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+class Category(models.Model):
+    Name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.Name
