a5394077e8cc4f2f9a9f841a8bce3529ebde850e
Xavier Kibet <xavierkibet@gmail.com>
2018-07-19 22:09:07 +0300

    Add search route

---
+++ imageshare/mainshare/views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from .models import *
+from django.shortcuts import render,get_object_or_404
+
+# Create your views here.
+def home(request):
+    images = Image.objects.all()
+    categories = Category.objects.all()
+    return render(request,'home.html',locals())
+
+def search(request):
+    searchstring=request.POST['search']
+    images=Image.search_image(searchstring)
+    name=searchstring
+    title=name
+    return render(request,'results.html',locals())
