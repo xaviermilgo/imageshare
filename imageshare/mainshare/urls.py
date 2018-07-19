9db1c8e5cce19ab5179d12a835797c8d17279728
Xavier Kibet <xavierkibet@gmail.com>
2018-07-19 21:48:59 +0300

    Add main routes to mainshre app

--- imageshare/mainshare/urls.py
+++ imageshare/mainshare/urls.py
@@ -2,3 +2,8 @@ from django.conf.urls import url,include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^$',views.home),
+    url(r'^search$',views.search),
+]
